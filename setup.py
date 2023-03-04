da677bc839fe8500d34478df90ba9fe520a7588b
eapuertoc <eapc@ier.unam.mx>
2023-03-03 20:57:24 -0600

    Updated version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="DCEU Toolbox",
-    version="0.2",
+    version="0.2.1",
     description="Workshop toolbox for DIPAC THERMAL SOLUTIONS",
     author="MEng. Efraín Alonso Puerto",
     author_email="efrainpuerto@gmail.com",
